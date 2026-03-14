b679163176a9eafbdb3cb9d69d53f0feb180a9b7
Davi RF <vitor25davi30@gmail.com>
2026-03-14 13:22:57 -0300

    .venv é um saco

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,9 @@
-import requests as rq
+from requests import post
 
 img = input()
 url = 'http://localhost:8000/convert'
 
 with open(img, 'rb') as file:
     files = {'file': file}
-    response = rq.post(url, files=files)
+    response = post(url, files=files)
 print(response.json())
