a1a72e2a491c33e7f3a875d6383408714431a0a1
Davi RF <vitor25davi30@gmail.com>
2026-04-01 17:13:06 -0300

    SEU_IP_AQUI corrected

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -17,7 +17,7 @@ import InsertPhotoBtn from './components/InsertPhotoBtn'
 import CodeBox from './components/CodeBox'
 import SegmentedToggle from './components/SegmentedToggle'
 
-const ip = '10.3.152.15'
+const ip = 'SEU_IP_AQUI'
 const url = `http://${ip}:8000/convert`
 
 export default function App() {
