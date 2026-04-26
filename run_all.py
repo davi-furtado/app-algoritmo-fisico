f59e6b416362ba3ef38b63d16b7c9ddd8ff5de0d
Davi Reis Furtado <vitor25davi30@gmail.com>
2026-04-26 11:59:04 -0300

    improvements to run_all.py

--- run_all.py
+++ run_all.py
@@ -1,37 +1,20 @@
 from subprocess import Popen
 import socket as sc
 
-ip = sc.gethostbyname(sc.gethostname())
+s = sc.socket(sc.AF_INET, sc.SOCK_DGRAM)
+s.connect(('10.255.255.255', 1))
+ip = s.getsockname()[0]
+s.close()
+print(ip)
 
-envs = ['backend/', 'frontend/mobile/', 'frontend/web/']
-for env in envs:
-    f = open(env + '.env', 'w')
+with open('frontend/mobile/.env', 'w') as f:
     f.write(f'IP={ip}')
-    f.close()
 
-try:
-    Popen('uvicorn main:app --host 0.0.0.0', cwd='backend')
-except:
-    Popen(
-        'pip install -r requirements.txt && uvicorn main:app --host 0.0.0.0',
-        cwd='backend',
-        shell=True
-    )
+with open('frontend/web/.env', 'w') as f:
+    f.write(f'IP={ip}')
 
-try:
-    Popen('npx expo start --port 6000', cwd='frontend/mobile')
-except:
-    Popen(
-        'npm install && npx expo start --port 6000',
-        cwd='frontend/mobile',
-        shell=True
-    )
+Popen('uvicorn main:app --host 0.0.0.0', cwd='backend')
 
-try:
-    Popen('npm run dev -- --host 0.0.0.0 --port 4000', cwd='frontend/web')
-except:
-    Popen(
-        'npm install && npm run dev -- --host 0.0.0.0 --port 4000',
-        cwd='frontend/web',
-        shell=True
-    )
+Popen('npx expo start --port 6000', cwd='frontend/mobile')
+
+Popen('npm run dev -- --host 0.0.0.0 --port 4000', cwd='frontend/web')
